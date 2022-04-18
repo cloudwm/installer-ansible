81035a6f0238c04a5234ea18651e1edf974c68c7
Lior Malev <liorm@omc.co.il>
2022-04-18 12:23:09 +0300

    Removed quotes

--- startup-cwm.sh
+++ startup-cwm.sh
@@ -147,7 +147,7 @@ function appendServerDescriptionTXT() {
 # Update server description in CWM according to description file
 function updateServerDescription() {
 
-    curl --location -f -X PUT --retry-connrefused --retry 3 --retry-delay 2 -H "AuthClientId: ${CWM_APICLIENTID}" -H "AuthSecret: ${CWM_APISECRET}" "https://$CWM_URL/svc/server/$CWM_UUID/description" --data-urlencode $'description='"$1"
+    curl --location -f -X PUT --retry-connrefused --retry 3 --retry-delay 2 -H "AuthClientId: ${CWM_APICLIENTID}" -H "AuthSecret: ${CWM_APISECRET}" "https://$CWM_URL/svc/server/$CWM_UUID/description" --data-urlencode $'description='$1
 
     local exitCode=$?
     if [ $exitCode -ne 0 ]; then
